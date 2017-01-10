3c3cf07619dc93c4b9d1797f4486194c5c8010e4
ron dyar <ron@fromex.net>
2017-01-09 19:37:45 -0800

    removed display variable and put them directly in the funtions. Changed innerHTML to textContent

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -15,33 +15,6 @@ var stabCostBox = document.querySelector('#stabCostBox');
 var stabLitersBox = document.querySelector('#stabLitersBox');
 var stabRepRate = document.querySelector('#stabRepRate');
 
-//used to display paper costs 
-var costPIpaper = document.querySelector('#costPIpaper');
-var costPFpaper = document.querySelector('#costPFpaper');
-var cost4x6paper = document.querySelector('#cost4x6paper');
-var cost5x7paper = document.querySelector('#cost5x7paper');
-var cost8x10paper = document.querySelector('#cost8x10paper');
-var cost11x14paper = document.querySelector('#cost11x14paper');
-var cost16x20paper = document.querySelector('#cost16x20paper');
-var cost10kpaper = document.querySelector('#cost10kpaper');
-// used to display chem costs
-var costPIchemicals = document.querySelector('#costPIchemicals');
-var costPFchemicals = document.querySelector('#costPFchemicals');
-var cost4x6chemicals = document.querySelector('#cost4x6chemicals');
-var cost5x7chemicals = document.querySelector('#cost5x7chemicals');
-var cost8x10chemicals = document.querySelector('#cost8x10chemicals');
-var cost11x14chemicals = document.querySelector('#cost11x14chemicals');
-var cost16x20chemicals = document.querySelector('#cost16x20chemicals');
-var cost10kchemicals = document.querySelector('#cost10kchemicals');
-//used to display total costs
-var totalPI = document.querySelector('#totalPI');
-var totalPF = document.querySelector('#totalPF');
-var total4x6 = document.querySelector('#total4x6');
-var total5x7 = document.querySelector('#total5x7');
-var total8x10 = document.querySelector('#total8x10');
-var total11x14 = document.querySelector('#total11x14');
-var total16x20 = document.querySelector('#total16x20');
-var total10k = document.querySelector('#total10k');
 //used to display sqf and sqi
 var displaySqf = document.querySelector('#sfpr');
 var displaySqi = document.querySelector('#sipr');
@@ -66,33 +39,33 @@ function update (){
 	var paperCostPerInch = calcPaperCostPerInch();
 	var chemCostPerInch = (devCostPerInch+blxCostPerInch+stabCostPerInch);
 		// paper costs
-		costPIpaper.innerHTML = paperCostPerInch;
-		costPFpaper.innerHTML = rounder((paperCostPerInch* 144),4)
-		cost4x6paper.innerHTML = rounder(paperCostPerInch* 24,4)
-		cost5x7paper.innerHTML = rounder(paperCostPerInch* 35,4)
-		cost8x10paper.innerHTML = rounder(paperCostPerInch* 80,4)
-		cost11x14paper.innerHTML = rounder(paperCostPerInch* 151,4)
-		cost16x20paper.innerHTML = rounder(paperCostPerInch* 320,4)
-		cost10kpaper.innerHTML = rounder(paperCostPerInch* 240000,4)
+		document.querySelector('#costPIpaper').textContent = paperCostPerInch;
+		document.querySelector('#costPFpaper').textContent = rounder((paperCostPerInch* 144),4)
+		document.querySelector('#cost4x6paper').textContent = rounder(paperCostPerInch* 24,4)
+		document.querySelector('#cost5x7paper').textContent = rounder(paperCostPerInch* 35,4)
+		document.querySelector('#cost8x10paper').textContent = rounder(paperCostPerInch* 80,4)
+		document.querySelector('#cost11x14paper').textContent = rounder(paperCostPerInch* 151,4)
+		document.querySelector('#cost16x20paper').textContent = rounder(paperCostPerInch* 320,4)
+		document.querySelector('#cost10kpaper').textContent = rounder(paperCostPerInch* 240000,4)
 		//chem costs
-		costPIchemicals.innerHTML = rounder(chemCostPerInch,4);
-		costPFchemicals.innerHTML =  rounder(chemCostPerInch * 144,4);
-		cost4x6chemicals.innerHTML =  rounder(chemCostPerInch * 24,4);
-		cost5x7chemicals.innerHTML =  rounder(chemCostPerInch * 35,4);
-		cost8x10chemicals.innerHTML =  rounder(chemCostPerInch * 80,4);
-		cost11x14chemicals.innerHTML =  rounder(chemCostPerInch * 151,4);
-		cost16x20chemicals.innerHTML =  rounder(chemCostPerInch * 320,4);
-		cost10kchemicals.innerHTML =  rounder(chemCostPerInch * 240000,4);
+		document.querySelector('#costPIchemicals').textContent = rounder(chemCostPerInch,4);
+		document.querySelector('#costPFchemicals').textContent =  rounder(chemCostPerInch * 144,4);
+		document.querySelector('#cost4x6chemicals').textContent =  rounder(chemCostPerInch * 24,4);
+		document.querySelector('#cost5x7chemicals').textContent =  rounder(chemCostPerInch * 35,4);
+		document.querySelector('#cost8x10chemicals').textContent =  rounder(chemCostPerInch * 80,4);
+		document.querySelector('#cost11x14chemicals').textContent =  rounder(chemCostPerInch * 151,4);
+		document.querySelector('#cost16x20chemicals').textContent =  rounder(chemCostPerInch * 320,4);
+		document.querySelector('#cost10kchemicals').textContent =  rounder(chemCostPerInch * 240000,4);
 		//total costs
 		var totalPerInch = paperCostPerInch + chemCostPerInch;
-		totalPI.innerHTML = rounder(totalPerInch,4);
-		totalPF.innerHTML = rounder((totalPerInch*144),3);
-		total4x6.innerHTML = rounder((totalPerInch*24),3);
-		total5x7.innerHTML = rounder((totalPerInch*35),3);
-		total8x10.innerHTML = rounder((totalPerInch*80),3);
-		total11x14.innerHTML = rounder((totalPerInch*151),3);
-		total16x20.innerHTML = rounder((totalPerInch*320),3);
-		total10k.innerHTML = rounder((totalPerInch*240000),3);
+		document.querySelector('#totalPI').textContent = rounder(totalPerInch,4);
+		document.querySelector('#totalPF').textContent = rounder((totalPerInch*144),3);
+		document.querySelector('#total4x6').textContent = rounder((totalPerInch*24),3);
+		document.querySelector('#total5x7').textContent = rounder((totalPerInch*35),3);
+		document.querySelector('#total8x10').textContent = rounder((totalPerInch*80),3);
+		document.querySelector('#total11x14').textContent = rounder((totalPerInch*151),3);
+		document.querySelector('#total16x20').textContent = rounder((totalPerInch*320),3);
+		document.querySelector('#total10k').textContent = rounder((totalPerInch*240000),3);
 
 		console.groupCollapsed("results")
 		console.log("Paper Cost per Square Inch: ",paperCostPerInch);
@@ -107,8 +80,8 @@ function update (){
 	function calcSqi () {
 		var totalSqf = paperWidth.value/12 *paperLength.value;
 		var paperSqi = totalSqf*144
-		displaySqf.innerHTML = Number((totalSqf).toFixed(2));
-		displaySqi.innerHTML = paperSqi;
+		displaySqf.textContent = Number((totalSqf).toFixed(2));
+		displaySqi.textContent = paperSqi;
 		return paperSqi;
 	}
 
@@ -124,8 +97,7 @@ function update (){
 	}
 
 	function rounder (num, limit){
-		var rounded = Number((num).toFixed(limit));
-		return rounded;
+	    return rounded = Number((num).toFixed(limit));
 	}
 
 //if enter is pressed remove focus
@@ -142,7 +114,7 @@ gallons.addEventListener('change', gallons2liters);
 function gallons2liters (){
 	var liters = this.value * 3.785;
 	var displayLiters = document.querySelector('#liters');
-	displayLiters.innerHTML = rounder(liters,2);
+	displayLiters.textContent = rounder(liters,2);
 }
 
 var mlm = document.querySelector('#mlm');
@@ -152,5 +124,5 @@ function mlm2mft (){
 	var mft = this.value / 10.7639;
 
 	var displaymft = document.querySelector('#mft');
-	displaymft.innerHTML = rounder(mft,2);
+	displaymft.textContent = rounder(mft,2);
 }
